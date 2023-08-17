b4a07e3046b349d15f04a53d96da9f86e6ace631
Eugene_dev <lointainyyy@gmail.com>
2023-08-17 15:27:24 +0300

    changed font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import '@styles/globals.css';
-import { Inter } from 'next/font/google';
+import { Inter, Montserrat } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const font = Montserrat({ subsets: ['latin'] });
 
 import '@styles/main.scss';
 
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={font.className}>{children}</body>
 		</html>
 	);
 }
