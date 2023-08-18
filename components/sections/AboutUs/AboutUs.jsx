25a22691f63aae24b52acd0c7565e7244a48af00
Eugene_dev <lointainyyy@gmail.com>
2023-08-18 11:19:39 +0300

    add aboutUs section

---
+++ components/sections/AboutUs/AboutUs.jsx
@@ -0,0 +1,74 @@
+import Container from '@components/fields/Container/Container';
+import styles from './AboutUs.module.scss';
+
+import { Alegreya } from 'next/font/google';
+
+const font = Alegreya({
+	subsets: ['latin']
+});
+
+const AboutUs = () => {
+	const services = [
+		{
+			title: 'Sustainable organic production'
+		},
+		{
+			title: '100% vegan product'
+		},
+		{
+			title: 'Premium quality CBD products'
+		},
+		{
+			title: 'Free shipping for orders over $50'
+		},
+		{
+			title: '30 days free return period'
+		},
+		{
+			title: '24/7 online customer support'
+		}
+	];
+
+	return (
+		<section
+			className={styles.section}
+			style={{
+				backgroundImage: `url('')`
+			}}
+		>
+			<Container>
+				<div className={`${styles.headline} ${font.className}`}>
+					<img src="assets/images/leaf.svg" alt="leaf-img" className={styles.headlineImg} />
+					<h2>What is Seven Vital CBD?</h2>
+				</div>
+				<div className={styles.contentWrapper}>
+					<div className={styles.imgWrapper}>
+						<img src="assets/images/aboutUs-first.png" alt="aboutus-first-img" className={styles.imgFirst} />
+						<img src="assets/images/aboutUs-second.png" alt="aboutus-second-img" className={styles.imgSecond} />
+					</div>
+					<div className={styles.content}>
+						<p className={styles.desc}>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum amet itaque, consequuntur vitae numquam eligendi, repellat earum
+							assumenda sint beatae repudiandae incidunt aperiam voluptatem accusantium, quas omnis quis ex porro?
+						</p>
+						<ul className={styles.list}>
+							{services.map((service, i) => {
+								return (
+									<li className={styles.item} key={`${i} ${service.title}`}>
+										<i className={styles.itemIcon}>
+											<img src="/assets/icons/check.svg" alt="check-icon" />
+										</i>
+										<span className={styles.itemTitle}>{service.title}</span>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
+
+export default AboutUs;
+
--- pages/HomePage/HomePage.jsx
+++ pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import Layout from '@components/layout/Layout';
+import AboutUs from '@components/sections/AboutUs/AboutUs';
 import Hero from '@components/sections/Hero/Hero';
 import React from 'react';
 
 const HomePage = () => {
 	return (
 		<Layout title={'Home'} description={''}>
-			<Hero />
+			<main>
+				<Hero />
+				<AboutUs />
+			</main>
 		</Layout>
 	);
 };
