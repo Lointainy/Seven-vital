20a3bd8e08a41fb1f5412a0d688874dfc4ba5379
Eugene_dev <lointainyyy@gmail.com>
2023-08-23 15:11:54 +0300

    add links

---
+++ utils/links.js
@@ -0,0 +1,16 @@
+const links = [
+	{
+		path: 'shop',
+		title: 'shop'
+	},
+	{
+		path: 'account',
+		title: 'account'
+	},
+	{
+		path: 'contact',
+		title: 'contact'
+	}
+];
+
+export default links;
