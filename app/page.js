14ef15039562e6d3e0283e2fda0136aca4f05138
Eugene_dev <lointainyyy@gmail.com>
2023-08-25 10:21:40 +0300

    add preload hero img

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 import Layout from '@components/layout/Layout';
 import HomePage from '@pages/HomePage/HomePage';
+import Head from 'next/head';
 
 export default function Home() {
-	return <HomePage />;
+	return (
+		<>
+			<Head>
+				<link rel="preload" href={'assets/images/hero-background.webp'} as="image" />
+			</Head>
+			<HomePage />
+		</>
+	);
 }
 
